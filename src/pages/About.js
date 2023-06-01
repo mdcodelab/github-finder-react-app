90a074b5a4044459692a47eda2502b96e34e1ccf
miha11-hash <d_mihaela@msn.com>
2023-06-02 01:56:16 +0300

    some styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
       <h1>GitHub Finder</h1>
       <p>A React app to search GitHub profiles and see profile details.</p>
       <p>Version: 1.0.0</p>
-      <p>Layout by Mihaela Diaconu</p>
+      <a href="https://github.com/mdcodelab">Layout by Mihaela Diaconu</a>
     </div>
   );
 }
